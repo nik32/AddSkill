c295426feff450f0cae7b2791bf1a881ff6ec0c1
nik32 <51018572+nik32@users.noreply.github.com>
2020-11-14 14:12:35 +0530

    64 ms

---
+++ solutions/173. Binary Search Tree Iterator.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+private:
+    stack<TreeNode *> path;
+public:
+    BSTIterator(TreeNode* root) {
+        for(TreeNode *node = root ; node != NULL ; node = node->left)
+            path.push(node);
